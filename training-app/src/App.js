a726c2b973ba0aa7fb7f99335f16eb901839a501
Doom <vnagorniyrnd@gmail.com>
2021-12-17 21:28:18 +0300

    Знакомство со state

--- training-app/src/App.js
+++ training-app/src/App.js
@@ -1,14 +1,32 @@
+import { Component } from 'react';
 import './App.css';
 
-function WhoAmI({ name, surname, link }) {
-  return (
-    <div>
-      <h1>
-        My name is {name}, surname - {surname}
-      </h1>
-      <a href={link}>My profile</a>
-    </div>
-  );
+class WhoAmI extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      years: 27,
+    };
+  }
+
+  nextYear = () => {
+    this.setState((state) => ({
+      years: state.years + 1,
+    }));
+  };
+
+  render() {
+    const { name, surname, link } = this.props;
+    return (
+      <div>
+        <button onClick={this.nextYear}>+++</button>
+        <h1>
+          My name is {name}, surname - {surname}, age - {this.state.years}
+        </h1>
+        <a href={link}>My profile</a>
+      </div>
+    );
+  }
 }
 
 function App() {
