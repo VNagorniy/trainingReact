a0080e82379834935cd6c1bb62a80b4840410240
Doom <vnagorniyrnd@gmail.com>
2021-12-18 16:46:12 +0300

    События, рассмотрение контекста вызова

--- training-app/src/App.js
+++ training-app/src/App.js
@@ -6,6 +6,7 @@ class WhoAmI extends Component {
     super(props);
     this.state = {
       years: 27,
+      position: '',
     };
   }
 
@@ -15,15 +16,27 @@ class WhoAmI extends Component {
     }));
   };
 
+  commitInputChanges = (e) => {
+    this.setState({
+      position: e.target.value,
+    });
+  };
+
   render() {
     const { name, surname, link } = this.props;
+    const { position, years } = this.state;
     return (
       <div>
         <button onClick={this.nextYear}>+++</button>
         <h1>
-          My name is {name}, surname - {surname}, age - {this.state.years}
+          My name is {name}, surname - {surname}, age - {years}, position -
+          {position}
         </h1>
         <a href={link}>My profile</a>
+        <form>
+          <span>Введите должность</span>
+          <input type='text' onChange={this.commitInputChanges} />
+        </form>
       </div>
     );
   }
