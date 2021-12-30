7dea602b74bb3d3b02c781755733470e77975104
Doom <vnagorniyrnd@gmail.com>
2021-12-31 00:33:47 +0300

    реализация styled components

--- training-app/src/App.js
+++ training-app/src/App.js
@@ -2,6 +2,34 @@ import { Component } from 'react';
 import styled from 'styled-components';
 import './App.css';
 
+const EmpItem = styled.div`
+  padding: 20px;
+  margin-bottom: 15px;
+  border-radius: 5px;
+  box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.2);
+  a {
+    display: block;
+    margin: 10px 0 10px 0;
+    color: ${(props) => (props.active ? 'orange' : 'black')};
+  }
+  input {
+    display: block;
+    margin-top: 10px;
+  }
+`;
+
+const Header = styled.h2`
+  font-size: 22px;
+`;
+
+export const Button = styled.button`
+  display: block;
+  padding: 5px 15px;
+  background-color: gold;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.2);
+`;
+
 class WhoAmI extends Component {
   constructor(props) {
     super(props);
@@ -27,17 +55,17 @@ class WhoAmI extends Component {
     const { name, surname, link } = this.props;
     const { position, years } = this.state;
     return (
-      <>
-        <button onClick={this.nextYear}>+++</button>
-        <h1>
+      <EmpItem active>
+        <Button onClick={this.nextYear}>+++</Button>
+        <Header>
           My name is {name}, surname - {surname}, age - {years}, position -{position}
-        </h1>
+        </Header>
         <a href={link}>My profile</a>
         <form>
           <span>Введите должность</span>
           <input type='text' onChange={this.commitInputChanges} />
         </form>
-      </>
+      </EmpItem>
     );
   }
 }
--- training-app/src/index.js
+++ training-app/src/index.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import styled from 'styled-components';
+import { Button } from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const BigButton = styled(Button)`
+  margin: 0 auto;
+  width: 245px;
+  text-align: center;
+`;
+
+ReactDOM.render(
+  <StrictMode>
+    <App />
+    <BigButton as='a'>Отправить отчёт</BigButton>
+  </StrictMode>,
+  document.getElementById('root')
+);
